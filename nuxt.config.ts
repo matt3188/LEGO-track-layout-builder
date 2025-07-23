dbfa994ac23357874309cd5e02a777f7b08366b0
Matt Coleman <matt.coleman@protonmail.com>
2025-07-23 08:15:56 +0100

    feat: install @nuxt/ui and upgrade Tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
+  css: ['~/assets/css/main.css'],
   modules: [
-    '@nuxtjs/tailwindcss'
+    '@nuxt/ui'
   ],
   app: {
     baseURL: process.env.NODE_ENV === 'production' ? '/LEGO-track-layout-builder/' : '/',
