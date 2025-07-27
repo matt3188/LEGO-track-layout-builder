fd0b8d87a4bde6bfe02babff43ac8bf7cfd353a2
Matt Coleman <matt.coleman@protonmail.com>
2025-07-27 14:58:26 +0100

    feat: change border width of help button

--- components/HelpButton.vue
+++ components/HelpButton.vue
@@ -7,11 +7,11 @@ defineEmits<Emits>();
 </script>
 
 <template>
-  <button 
-    @click="$emit('click')"
+  <UButton 
     title="Show keyboard shortcuts"
-    class="fixed bottom-4 right-4 w-10 h-10 rounded-full border-2 border-gray-800 bg-white text-xl font-bold cursor-pointer flex items-center justify-center shadow-lg transition-all duration-200 hover:bg-gray-800 hover:text-white hover:scale-110"
+    class="text-black fixed bottom-4 right-4 w-10 h-10 rounded-full border-1 border-gray-800 bg-white text-xl font-bold cursor-pointer flex items-center justify-center shadow-lg transition-all duration-200 hover:bg-gray-800 hover:text-white hover:scale-110"
+    @click="$emit('click')"
   >
     ?
-  </button>
+  </UButton>
 </template>
