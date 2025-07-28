baccd4ff7146bf2d2311612acd432e452686fae2
Matt Coleman <matt.coleman@protonmail.com>
2025-07-28 22:04:48 +0100

    refactor: button styles

--- components/HelpButton.vue
+++ components/HelpButton.vue
@@ -10,7 +10,7 @@ defineEmits<Emits>();
   <div class="fixed bottom-4 right-4">
     <UButton 
       title="Show keyboard shortcuts"
-      class="text-black  w-10 h-10 rounded-full border-1 border-gray-800 bg-white text-xl font-bold cursor-pointer flex items-center justify-center shadow-lg transition-all duration-200 hover:bg-gray-800 hover:text-white hover:scale-110"
+      class="text-black w-10 h-10 rounded-full border-1 border-gray-800 bg-white text-xl font-bold cursor-pointer flex items-center justify-center shadow-lg transition-all duration-200 hover:bg-gray-800 hover:text-white hover:scale-110"
       @click="$emit('click')"
     >
       ?
